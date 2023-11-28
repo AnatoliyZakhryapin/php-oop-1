1ba27d7481cebc452a391be2dfe3626f79fd0f34
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-28 16:32:45 +0100

    ADD objects of Production

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+
+class Production {
+    public $title;
+    public $language;
+    public $subtitle;
+    public $rating;
+
+    function __construct( String $_title, Array $_language, Bool $_subtitle, Int $_rating){
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->subtitle = $_subtitle;
+        $this->setRating($_rating);
+    }
+
+    public function setRating($vote){
+        if(is_numeric($vote) && $vote >= 0){
+            $this->rating = intval($vote);
+        } else {
+            $this->rating = 0;
+            var_dump('Il parametro $rate non è corretto');
+        }
+    }
+
+};
+
+$ScaryMovie = new Production ('Scary movie', ['italiano', 'inglese'], true, 8,5);
+$ArmorOfGod = new Production ('Armor of God', ['inglese'], true, 10);
+var_dump($ScaryMovie, $ArmorOfGod );
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
