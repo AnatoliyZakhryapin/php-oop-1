3e8403e2f85cb403eb13780f65e2488768d7e60c
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-29 16:31:07 +0100

    ADD Serie.php

--- Models/Serie.php
+++ Models/Serie.php
@@ -1,5 +1,28 @@
 <?php
 
-class Serie {
-    
+require_once __DIR__ . './Production.php';
+
+class Serie extends Production {
+    public $season;
+
+    function __construct(
+        String $_title, 
+        Array $_language, 
+        Bool $_subtitle, 
+        Array $_actors, 
+        Int $_rating, 
+        String $_src_poster,
+        Int $_season
+    ) {
+        parent::__construct($_title, $_language, $_subtitle, $_actors, $_rating, $_src_poster);
+        $this->setSeason($_season);
+    }
+
+    public function setSeason($_season) {
+        $this->season = $_season;
+    }
+
+    public function getSeason(){
+        return $this->season;
+    }
 }
