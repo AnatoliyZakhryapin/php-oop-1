3f742a75765841c4519834c149d62056bf762f1d
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-29 16:01:51 +0100

    ADD content to movie.php

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,5 +1,39 @@
 <?php
 
-class Movie {
-    
+require_once __DIR__ . './Production.php';
+
+class Movie extends Production {
+    public $profit;
+    public $duration;
+
+    function __construct(
+        String $_title, 
+        Array $_language, 
+        Bool $_subtitle, 
+        Array $_actors, 
+        Int $_rating, 
+        String $_src_poster,
+        Int $_profit,
+        String $_duration
+    ) {
+        parent::__construct($_title, $_language, $_subtitle, $_actors, $_rating, $_src_poster);
+        $this->setProfit($_profit);
+        $this-> setDuration($_duration);
+    }
+
+    public function setProfit($_profit) {
+        $this->profit = $_profit;
+    }
+
+    public function getProfit(){
+        return $this->profit;
+    }
+
+    public function setDuration($_duration) {
+        $this->duration = $_duration;
+    }
+
+    public function getDuration(){
+        return $this->duration;
+    }
 }
