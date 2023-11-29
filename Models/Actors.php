e88271f706441deaebbde9fade8fcb377275edef
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-29 17:10:54 +0100

    Delete isMovie function

--- Models/Actors.php
+++ Models/Actors.php
@@ -1,12 +1,14 @@
 <?php
 
-class Actor {
+class Actor
+{
     public $name;
     public $full_name;
     public $last_name;
     public $birth_day;
 
-    function __construct(String $_name, String $_last_name, String $_birth_day) {
+    function __construct(String $_name, String $_last_name, String $_birth_day)
+    {
         $this->name = $_name;
         $this->last_name = $_last_name;
         $this->full_name = $_name . ' ' . $_last_name;
